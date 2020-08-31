093dec7142db4ba1904e1adc3adb2a0c3d7bdcaa
siramkalyan <siramkalyan@gmail.com>
2020-08-31 11:33:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.profile;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
+    Button t_button,c_button;
+    int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView=findViewById(R.id.tv);
+        t_button=findViewById(R.id.toast_button);
+        c_button=findViewById(R.id.count_button);
+        if (savedInstanceState!=null && savedInstanceState.containsKey("key_name")){
+            c= savedInstanceState.getInt("key_name");
+            mTextView.setText(String.valueOf(c));
+
+        }
+
+        t_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Welcome to App developmemt",Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        c_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c++;
+                mTextView.setText(String.valueOf(c));
+                Toast.makeText(getApplicationContext(),"Count is incremented to"+String.valueOf(c),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("key_name",c);
+
+    }
+}
